18a7a7c820dd0995172494083cd040132777ecbd
Robson Cassiano <robson.cordeiro777@gmail.com>
2022-09-02 17:56:49 -0300

    queue performance test

---
+++ triggerMultipleRequests.sh
@@ -0,0 +1,10 @@
+#while true ; do
+#  curl -X POST --location "http://localhost:8083/api/v1/customers" \
+#      -H "Content-Type: application/json" \
+#      -d "{
+#            \"firstName\": \"Fulano\",
+#            \"lastname\": \"da Silva\",
+#            \"email\": \"e333lh@gdef333md.com\"
+#          }"
+#done
+
